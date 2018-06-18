16bfbaab9e0a780ee1815392c7c2ac6f760f779d
Mat Trudel <mat@geeky.net>
2018-06-18 10:14:17 -0400

    Read (but don't yet implement) 7 segment extended crontabs

--- lib/cron_parser.rb
+++ lib/cron_parser.rb
@@ -6,7 +6,7 @@ require 'date'
 class CronParser
   # internal "mutable" time representation
   class InternalTime
-    attr_accessor :year, :month, :day, :hour, :min
+    attr_accessor :year, :month, :day, :hour, :min, :sec
     attr_accessor :time_source
 
     def initialize(time,time_source = Time)
@@ -15,16 +15,17 @@ class CronParser
       @day = time.day
       @hour = time.hour
       @min = time.min
+      @sec = time.sec
 
       @time_source = time_source
     end
 
     def to_time
-      time_source.local(@year, @month, @day, @hour, @min, 0)
+      time_source.local(@year, @month, @day, @hour, @min, @sec)
     end
 
     def inspect
-      [year, month, day, hour, min].inspect
+      [year, month, day, hour, min, sec].inspect
     end
   end
 
@@ -246,15 +247,30 @@ class CronParser
 
   def time_specs
     @time_specs ||= begin
-      # tokens now contains the 5 fields
       tokens = substitute_parse_symbols(@source).split(/\s+/)
-      {
-        :minute => parse_element(tokens[0], 0..59), #minute
-        :hour   => parse_element(tokens[1], 0..23), #hour
-        :dom    => parse_element(tokens[2], 1..31), #DOM
-        :month  => parse_element(tokens[3], 1..12), #mon
-        :dow    => parse_element(tokens[4], 0..6)  #DOW
-      }
+      # tokens now contains the 5 or 7 fields
+      
+      if tokens.count <= 6
+        {
+          :second => parse_element("0", 0..59),       #second
+          :minute => parse_element(tokens[0], 0..59), #minute
+          :hour   => parse_element(tokens[1], 0..23), #hour
+          :dom    => parse_element(tokens[2], 1..31), #DOM
+          :month  => parse_element(tokens[3], 1..12), #mon
+          :dow    => parse_element(tokens[4], 0..6),  #DOW
+          :year   => parse_element("*", 1900..3000)   #year
+        }
+      else
+        {
+          :second => parse_element(tokens[0], 0..59), #second
+          :minute => parse_element(tokens[1], 0..59), #minute
+          :hour   => parse_element(tokens[2], 0..23), #hour
+          :dom    => parse_element(tokens[3], 1..31), #DOM
+          :month  => parse_element(tokens[4], 1..12), #mon
+          :dow    => parse_element(tokens[5], 0..6),  #DOW
+          :year   => parse_element(tokens[6], 1900..3000)  #year
+        }
+      end
     end
   end
 
@@ -291,7 +307,7 @@ class CronParser
       raise ArgumentError, 'not a valid cronline'
     end
     source_length = @source.split(/\s+/).length
-    unless source_length >= 5 && source_length <= 6
+    unless (source_length >= 5 && source_length <= 6) || (source_length >= 7 && source_length <= 8)
       raise ArgumentError, 'not a valid cronline'
     end
   end
